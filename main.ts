4972af6d916a9e528985b174e5b5f10fd5fd83fe
27kwhite <27kwhite@aos92.org>
2025-09-14 14:29:57 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,19 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.vy = -150
     }
 })
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vx > 0) {
+        for (let index = 0; index < 10; index++) {
+            mySprite.x += 3
+            pause(15)
+        }
+    } else if (mySprite.vx < 0) {
+        for (let index = 0; index < 10; index++) {
+            mySprite.x += -3
+            pause(15)
+        }
+    }
+})
 let mySprite: Sprite = null
 scene.setBackgroundColor(6)
 tiles.setCurrentTilemap(tilemap`level1`)
