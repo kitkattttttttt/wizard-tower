b46f85b9727c6e564765223802d035dd596c2ffd
27kwhite <27kwhite@aos92.org>
2025-09-16 08:32:06 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -36,6 +36,41 @@ namespace myImages {
 ................
 ................
 `;
+            case "image2":
+            case "dashEmpty":return img`
+f f f f f f f f f f f f f f f f f f 
+f . . . . . . . . . . . . . . . . f 
+f . . . . . . . . . . . . . . . . f 
+f f f f f f f f f f f f f f f f f f 
+`;
+            case "image3":
+            case "dashOne":return img`
+f f f f f f f f f f f f f f f f f f 
+f 2 2 2 2 . . . . . . . . . . . . f 
+f 2 2 2 2 . . . . . . . . . . . . f 
+f f f f f f f f f f f f f f f f f f 
+`;
+            case "image4":
+            case "dashTwo":return img`
+f f f f f f f f f f f f f f f f f f 
+f 4 4 4 4 4 4 4 4 . . . . . . . . f 
+f 4 4 4 4 4 4 4 4 . . . . . . . . f 
+f f f f f f f f f f f f f f f f f f 
+`;
+            case "image5":
+            case "dashThree":return img`
+f f f f f f f f f f f f f f f f f f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 . . . . f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 . . . . f 
+f f f f f f f f f f f f f f f f f f 
+`;
+            case "image6":
+            case "dashFour":return img`
+f f f f f f f f f f f f f f f f f f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f f f f f f f f f f f f f f f f f f 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,24 +1,65 @@
+namespace SpriteKind {
+    export const info = SpriteKind.create()
+}
+// Single Jump
+// 
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
         mySprite.vy = -150
     }
 })
+// Dash/Dash Meter
+// 
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.vx > 0) {
-        for (let index = 0; index < 10; index++) {
-            mySprite.x += 3
-            pause(15)
-        }
-    } else if (mySprite.vx < 0) {
-        for (let index = 0; index < 10; index++) {
-            mySprite.x += -3
-            pause(15)
+    if (dash == 100) {
+        if (mySprite.vx != 0) {
+            if (mySprite.vx > 0) {
+                for (let index = 0; index < 10; index++) {
+                    mySprite.x += 3
+                    pause(15)
+                }
+            } else if (mySprite.vx < 0) {
+                for (let index = 0; index < 10; index++) {
+                    mySprite.x += -3
+                    pause(15)
+                }
+            }
+            dash = 0
+            dashMeter = sprites.create(assets.image`dashEmpty`, SpriteKind.info)
+            dashMeter.setPosition(12, 4)
+            for (let index = 0; index < 25; index++) {
+                dash += 1
+                pause(10)
+            }
+            dashMeter.setImage(assets.image`dashOne`)
+            for (let index = 0; index < 25; index++) {
+                dash += 1
+                pause(10)
+            }
+            dashMeter.setImage(assets.image`dashTwo`)
+            for (let index = 0; index < 25; index++) {
+                dash += 1
+                pause(10)
+            }
+            dashMeter.setImage(assets.image`dashThree`)
+            for (let index = 0; index < 25; index++) {
+                dash += 1
+                pause(10)
+            }
+            dashMeter.setImage(assets.image`dashFour`)
+            pause(1000)
+            sprites.destroy(dashMeter)
         }
     }
 })
+// Setup
+// 
+let dashMeter: Sprite = null
+let dash = 0
 let mySprite: Sprite = null
 scene.setBackgroundColor(6)
 tiles.setCurrentTilemap(tilemap`level1`)
 mySprite = sprites.create(assets.image`Apprentice`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 300
+dash = 100
