2ddfc09aa1576dcd6a00826ff435aa0c01dcb60e
27kwhite <27kwhite@aos92.org>
2025-10-02 08:13:53 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -58,7 +58,7 @@ f f f f f f f f f f f f f f f f f f
 . . . . . f f . . f f . . . . . 
 `;
             case "image7":
-            case "ApprenticeFaceLef":return img`
+            case "uwherg":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -75,28 +75,9 @@ f f f f f f f f f f f f f f f f f f
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`;
-            case "image9":
-            case "ApprenticeFaceLeft":return img`
-. . . . . . . . f f f . . . . . 
-. . . . . . . f 8 9 9 f . . . . 
-. . . . . . f f 8 8 9 f . . . . 
-. . . . . f f 8 8 8 f . f . . . 
-. . . . . f f 8 8 8 f . . . . . 
-. . . . f f 8 8 8 8 9 f . . . . 
-. . . . f 4 4 5 5 1 1 f . . . . 
-. . . f f f 8 8 8 8 8 9 f . . . 
-. . . . f d f d d d f f . . . . 
-. . . . f d 3 3 d d f . . . . . 
-. . . . . f f f f f . . . . . . 
-. . . . . f f 8 9 f . . . . . . 
-. . . . f 5 8 8 8 9 f . . . . . 
-. . . . f 4 8 8 8 9 f . . . . . 
-. . . . f f f f f f f . . . . . 
-. . . . . . . f f . . . . . . . 
 `;
             case "image10":
-            case "ApprenticeFaceRight2":return img`
+            case "ierw":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -115,7 +96,7 @@ f f f f f f f f f f f f f f f f f f
 . . . . . . . . . . . . . . . . 
 `;
             case "image11":
-            case "ApprenticeFaceRight0":return img`
+            case "oweifhgvwo":return img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -133,8 +114,27 @@ f f f f f f f f f f f f f f f f f f
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
+            case "image12":
+            case "ApprenticeFaceRight2":return img`
+. . . . . f f f . . . . . . . . 
+. . . . f f f 8 f . . . . . . . 
+. . . . f f 8 8 9 f . . . . . . 
+. . . f . f 8 8 8 9 f . . . . . 
+. . . . . f 8 8 8 9 f . . . . . 
+. . . . f f 8 8 8 8 9 f . . . . 
+. . . . f 4 4 5 5 1 1 f . . . . 
+. . . f f 8 8 8 8 8 9 9 f . . . 
+. . . . f f d d d f d f . . . . 
+. . . . . f d d 3 3 d f . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . f f 8 9 f . . . . . 
+. . . . . f f 8 8 8 5 f . . . . 
+. . . . . f f 8 8 8 4 f f . . . 
+. . . . . f f f f f f f f . . . 
+. . . . . . . f f . . . . . . . 
+`;
             case "image8":
-            case "ApprenticeFaceRight":return img`
+            case "ApprenticeFaceRight1":return img`
 . . . . . f f f . . . . . . . . 
 . . . . f f f 8 f . . . . . . . 
 . . . . f f 8 8 9 f . . . . . . 
@@ -152,24 +152,103 @@ f f f f f f f f f f f f f f f f f f
 . . . . . f f f f f f f . . . . 
 . . . . . . . f f . . . . . . . 
 `;
-            case "image12":
-            case "ApprenticeFaceRight1":return img`
-. . . . . f f f . . . . . . . . 
-. . . . f f f 8 f . . . . . . . 
-. . . . f f 8 8 9 f . . . . . . 
-. . . f . f 8 8 8 9 f . . . . . 
-. . . . . f 8 8 8 9 f . . . . . 
+            case "image9":
+            case "ApprenticeFaceLeft1":return img`
+. . . . . . . . f f f . . . . . 
+. . . . . . . f 8 9 9 f . . . . 
+. . . . . . f f 8 8 9 f . . . . 
+. . . . . f f 8 8 8 f . f . . . 
+. . . . . f f 8 8 8 f . . . . . 
 . . . . f f 8 8 8 8 9 f . . . . 
 . . . . f 4 4 5 5 1 1 f . . . . 
-. . . f f 8 8 8 8 8 9 9 f . . . 
-. . . . f f d d d f d f . . . . 
-. . . . . f d d 3 3 d f . . . . 
-. . . . . . f f f f f . . . . . 
-. . . . . . f f 8 9 f . . . . . 
-. . . . . f f 8 8 8 5 f . . . . 
-. . . . . f f 8 8 8 4 f f . . . 
-. . . . . f f f f f f f f . . . 
+. . . f f f 8 8 8 8 8 9 f . . . 
+. . . . f d f d d d f f . . . . 
+. . . . f d 3 3 d d f . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f 8 9 f . . . . . . 
+. . . . f 5 8 8 8 9 f . . . . . 
+. . . . f 4 8 8 8 9 f . . . . . 
+. . . . f f f f f f f . . . . . 
 . . . . . . . f f . . . . . . . 
+`;
+            case "image13":
+            case "ApprenticeFaceLeft2":return img`
+. . . . . . . . f f f . . . . . 
+. . . . . . . f 8 9 9 f . . . . 
+. . . . . . f f 8 8 9 f . . . . 
+. . . . . f f 8 8 8 f . f . . . 
+. . . . . f f 8 8 8 f . . . . . 
+. . . . f f 8 8 8 8 9 f . . . . 
+. . . . f 4 4 5 5 1 1 f . . . . 
+. . . f f f 8 8 8 8 8 9 f . . . 
+. . . . f d f d d d f f . . . . 
+. . . . f d 3 3 d d f . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f 8 9 f . . . . . . 
+. . . . f 5 8 8 8 9 f . . . . . 
+. . . f f 4 8 8 8 9 f . . . . . 
+. . . f f f f f f f f . . . . . 
+. . . . . . . f f . . . . . . . 
+`;
+            case "image19":
+            case "health6":return img`
+.ff.ff...ff.ff...ff.ff...ff.ff...ff.ff...ff.ff.
+f22f21f.f22f21f.f22f21f.f22f21f.f22f21f.f22f21f
+f22222f.f22222f.f22222f.f22222f.f22222f.f22222f
+fc2222f.fc2222f.fc2222f.fc2222f.fc2222f.fc2222f
+.fc22f...fc22f...fc22f...fc22f...fc22f...fc22f.
+..fcf.....fcf.....fcf.....fcf.....fcf.....fcf..
+...f.......f.......f.......f.......f.......f...
+`;
+            case "image18":
+            case "health5":return img`
+. f f . f f . . . f f . f f . . . f f . f f . . . f f . f f . . . f f . f f . 
+f 2 2 f 2 1 f . f 2 2 f 2 1 f . f 2 2 f 2 1 f . f 2 2 f 2 1 f . f 2 2 f 2 1 f 
+f 2 2 2 2 2 f . f 2 2 2 2 2 f . f 2 2 2 2 2 f . f 2 2 2 2 2 f . f 2 2 2 2 2 f 
+f c 2 2 2 2 f . f c 2 2 2 2 f . f c 2 2 2 2 f . f c 2 2 2 2 f . f c 2 2 2 2 f 
+. f c 2 2 f . . . f c 2 2 f . . . f c 2 2 f . . . f c 2 2 f . . . f c 2 2 f . 
+. . f c f . . . . . f c f . . . . . f c f . . . . . f c f . . . . . f c f . . 
+. . . f . . . . . . . f . . . . . . . f . . . . . . . f . . . . . . . f . . . 
+`;
+            case "image17":
+            case "health4":return img`
+. f f . f f . . . f f . f f . . . f f . f f . . . f f . f f . 
+f 2 2 f 2 1 f . f 2 2 f 2 1 f . f 2 2 f 2 1 f . f 2 2 f 2 1 f 
+f 2 2 2 2 2 f . f 2 2 2 2 2 f . f 2 2 2 2 2 f . f 2 2 2 2 2 f 
+f c 2 2 2 2 f . f c 2 2 2 2 f . f c 2 2 2 2 f . f c 2 2 2 2 f 
+. f c 2 2 f . . . f c 2 2 f . . . f c 2 2 f . . . f c 2 2 f . 
+. . f c f . . . . . f c f . . . . . f c f . . . . . f c f . . 
+. . . f . . . . . . . f . . . . . . . f . . . . . . . f . . . 
+`;
+            case "image16":
+            case "health3":return img`
+. f f . f f . . . f f . f f . . . f f . f f . 
+f 2 2 f 2 1 f . f 2 2 f 2 1 f . f 2 2 f 2 1 f 
+f 2 2 2 2 2 f . f 2 2 2 2 2 f . f 2 2 2 2 2 f 
+f c 2 2 2 2 f . f c 2 2 2 2 f . f c 2 2 2 2 f 
+. f c 2 2 f . . . f c 2 2 f . . . f c 2 2 f . 
+. . f c f . . . . . f c f . . . . . f c f . . 
+. . . f . . . . . . . f . . . . . . . f . . . 
+`;
+            case "image15":
+            case "health2":return img`
+. f f . f f . . . f f . f f . 
+f 2 2 f 2 1 f . f 2 2 f 2 1 f 
+f 2 2 2 2 2 f . f 2 2 2 2 2 f 
+f c 2 2 2 2 f . f c 2 2 2 2 f 
+. f c 2 2 f . . . f c 2 2 f . 
+. . f c f . . . . . f c f . . 
+. . . f . . . . . . . f . . . 
+`;
+            case "image14":
+            case "health1":return img`
+. f f . f f . 
+f 2 2 f 2 1 f 
+f 2 2 2 2 2 f 
+f c 2 2 2 2 f 
+. f c 2 2 f . 
+. . f c f . . 
+. . . f . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -14,10 +14,20 @@ controller.right.onEvent(ControllerButtonEvent.Released, function () {
     mySprite.setImage(assets.image`Apprentice`)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.setImage(assets.image`ApprenticeFaceRight`)
+    mySprite.setImage(assets.image`ApprenticeFaceRight1`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`magicEvil0`, function (sprite, location) {
+    if (isInvincible == false) {
+        Life += -1
+        scene.cameraShake(4, 500)
+        isInvincible = true
+        timer.after(1000, function () {
+            isInvincible = false
+        })
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.setImage(assets.image`ApprenticeFaceLeft`)
+    mySprite.setImage(assets.image`ApprenticeFaceLeft1`)
 })
 // Dash/Dash Meter
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -46,7 +56,7 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
             }
             dash = 0
             dashMeter = sprites.create(assets.image`dashEmpty`, SpriteKind.info)
-            dashMeter.setPosition(12, 4)
+            dashMeter.setPosition(12, 13)
             dashMeter.setFlag(SpriteFlag.RelativeToCamera, true)
             for (let index = 0; index < 25; index++) {
                 dash += 1
@@ -75,6 +85,7 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 // Setup
 let dashMeter: Sprite = null
+let isInvincible = false
 let dash = 0
 let mySprite: Sprite = null
 scene.setBackgroundColor(6)
@@ -85,3 +96,24 @@ mySprite.ay = 300
 scene.cameraFollowSprite(mySprite)
 dash = 100
 tiles.placeOnTile(mySprite, tiles.getTileLocation(5, 20))
+let Life = 6
+let lives = sprites.create(assets.image`health6`, SpriteKind.info)
+lives.setPosition(27, 6)
+lives.setFlag(SpriteFlag.RelativeToCamera, true)
+forever(function () {
+    if (Life == 6) {
+        lives.setImage(assets.image`health6`)
+    } else if (Life == 5) {
+        lives.setImage(assets.image`health5`)
+    } else if (Life == 4) {
+        lives.setImage(assets.image`health4`)
+    } else if (Life == 3) {
+        lives.setImage(assets.image`health3`)
+    } else if (Life == 2) {
+        lives.setImage(assets.image`health2`)
+    } else if (Life == 1) {
+        lives.setImage(assets.image`health1`)
+    } else {
+        game.gameOver(false)
+    }
+})
